1e76323c1ad16f483f57f197249d0d5ee2487738
Owen Reilly <opr002@bucknell.edu>
2024-06-21 10:27:00 -0700

    better prompts

--- extraPicker.py
+++ extraPicker.py
@@ -19,9 +19,10 @@ class ExtraPicker:
 
     def generate_extra_prompt(self, extra_type):
         extra_prompt_map = {
-            "Zombies": "(solo) piece (zombie-like:1.5), (decaying:1.3), (undead:1.4), (tattered clothing:1.2), (horrifying:1.2), (full body:1.2), (high resolution:1.0), (sharp focus:1.0), (realistic textures:1.3), (dramatic lighting:1.0)",
-            "Robots": "(solo) piece (robotic:1.5), (mechanical:1.3), (metallic:1.4), (futuristic:1.2), (high-tech:1.2), (full body:1.2), (high resolution:1.0), (sharp focus:1.0), (realistic textures:1.3), (dramatic lighting:1.0)",
-            "Aliens": "(solo) piece (alien-like:1.5), (otherworldly:1.3), (extraterrestrial:1.4), (bizarre:1.2), (unearthly:1.2), (full body:1.2), (high resolution:1.0), (sharp focus:1.0), (realistic textures:1.3), (dramatic lighting:1.0)"
+            "Normal": "(solo) piece (realistic:1.5), (detailed:1.3), (human-like:1.4), (wearing a chess costume:1.3), maintaining the shape and features of a {piece}, (full body:1.2), (high resolution:1.0), (sharp focus:1.0), (realistic textures:1.3), (dramatic lighting:1.0)",
+            "Zombies": "(solo) piece (realistic:1.5), (detailed:1.3), (human-like:1.4), (wearing a chess costume:1.3), (decaying:1.3), (undead:1.4), (tattered clothing:1.2), (horrifying:1.2), maintaining the shape and features of a {piece}, (full body:1.2), (high resolution:1.0), (sharp focus:1.0), (realistic textures:1.3), (dramatic lighting:1.0)",
+            "Robots": "(solo) piece (realistic:1.5), (detailed:1.3), (human-like:1.4), (wearing a chess costume:1.3), (robotic:1.5), (mechanical:1.3), (metallic:1.4), (futuristic:1.2), (high-tech:1.2), maintaining the shape and features of a {piece}, (full body:1.2), (high resolution:1.0), (sharp focus:1.0), (realistic textures:1.3), (dramatic lighting:1.0)",
+            "Aliens": "(solo) piece (realistic:1.5), (detailed:1.3), (human-like:1.4), (wearing a chess costume:1.3), (alien-like:1.5), (otherworldly:1.3), (extraterrestrial:1.4), (bizarre:1.2), (unearthly:1.2), maintaining the shape and features of a {piece}, (full body:1.2), (high resolution:1.0), (sharp focus:1.0), (realistic textures:1.3), (dramatic lighting:1.0)"
         }
         return (extra_prompt_map[extra_type],)
 

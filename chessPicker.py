41cb0354eedf2bddb1979ff829543346bad32625
Owen Reilly <opr002@bucknell.edu>
2024-06-24 10:18:52 -0700

    work from Sketches

--- chessPicker.py
+++ chessPicker.py
@@ -12,6 +12,7 @@ class ChessPiecePrompt:
         return {
             "required": {
                 "piece": (["King", "Queen", "Bishop", "Knight", "Rook", "Pawn"],),
+                "color": (["white", "black"],)
             }
         }
 
@@ -30,7 +31,7 @@ class ChessPiecePrompt:
             image_tensor = torch.tensor(image_array).permute(2, 0, 1).unsqueeze(0)
             return image_tensor
 
-    def generate_prompt(self, piece):
+    def generate_prompt(self, piece, color):
         prompt_map = {
             "King": "(solo) chess piece (realistic:2), (detailed:1.3), (human-like:1.4), (wearing a chess costume:1.3), (metallic sheen:0.7), (polished:1.2), (intricate engravings:1.1), maintaining the shape and features of a king, (full body:1.2), (high resolution:1.0), (sharp focus:1.0), (realistic textures:1.3), (dramatic lighting:1.0), (holding a sword:1.3)",
             "Queen": "(solo) chess piece (realistic:2), (detailed:1.3), (human-like:1.4), (wearing a chess costume:1.3), (metallic sheen:0.7), (polished:1.2), (intricate engravings:1.1), maintaining the shape and features of a queen, (full body:1.2), (high resolution:1.0), (sharp focus:1.0), (realistic textures:1.3), (dramatic lighting:1.0)",
@@ -40,7 +41,8 @@ class ChessPiecePrompt:
             "Pawn": "(solo) chess piece (realistic:2), (detailed:1.3), (human-like:1.4), (wearing a chess costume:1.3), (metallic sheen:0.7), (polished:1.2), (intricate engravings:1.1), maintaining the shape and features of a pawn, (full body:1.2), (high resolution:1.0), (sharp focus:1.0), (realistic textures:1.3), (dramatic lighting:1.0)"
         }
 
-        image_path = os.path.join(os.path.dirname(__file__), "images", f"{piece.lower()}.png")
+        image_folder = "whiteImages" if color == "white" else "blackImages"
+        image_path = os.path.join(os.path.dirname(__file__), image_folder, f"{piece.lower()}.png")
         image_tensor = self.load_image(image_path)
         return (prompt_map[piece], image_tensor)
 
